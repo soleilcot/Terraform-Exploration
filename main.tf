d646c7ac0f6566c3a1d32e82864be2fd5ad2c845
Soleil Cotterell <soleil@Soleils-MacBook-Pro.local>
2022-11-05 19:51:57 -0500

    Apply new security group to example EC2 instance

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
     user_data = <<-E0F
                 #!/bin/bash
