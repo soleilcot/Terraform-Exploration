c4ce2ae95a54e31e53565309f2facd691444b104
Soleil Cotterell <soleilari@gmail.com>
2022-11-05 21:28:35 -0500

    chore: Deploy ALB in front of ASG and update .gitignore

---
+++ alb.tf
@@ -0,0 +1,21 @@
+resource "aws_lb" "example" {
+    name = "terraform-alb-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "404: page not found"
+        status_code = 404
+      }
+    }
+}
