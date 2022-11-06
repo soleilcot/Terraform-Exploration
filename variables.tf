d04721fd49063b11489d3d874a43361e99f940d3
Soleil Cotterell <soleil@Soleils-MacBook-Pro.local>
2022-11-05 20:11:02 -0500

    Add output variable for public ip of example EC2 instance

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "server_port" {
     type = number
     default = 8080
 }
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP of the example EC2 instance"
+}
