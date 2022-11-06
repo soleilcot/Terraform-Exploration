6436b1b7e9b04a6701ed0472de3e0ee505d963c1
Soleil Cotterell <soleilari@gmail.com>
2022-11-06 10:05:01 -0600

    chore: Update key for asg s3 backend

--- asg/backend.tf
+++ asg/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
       bucket = "tf-learning-and-practice"
-      key = "global/s3/terraform.tfstate"
+      key = "global/asg/terraform.tfstate"
       region = "us-east-2"
       dynamodb_table = "tf_locking_table"
       encrypt = true
