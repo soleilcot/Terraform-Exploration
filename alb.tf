6fa40963ec017396eb17cb6a42b1630ced2ce3e8
Soleil Cotterell <soleilari@gmail.com>
2022-11-05 21:38:41 -0500

    Apply security group to alb to permit inbound HTTP traffic and all outbound traffic

--- alb.tf
+++ alb.tf
@@ -2,6 +2,7 @@ resource "aws_lb" "example" {
     name = "terraform-alb-example"
     load_balancer_type = "application"
     subnets = data.aws_subnets.default.ids
+    security_groups = [ aws_security_group.alb.id ]
 }
 
 resource "aws_lb_listener" "http" {
@@ -18,4 +19,27 @@ resource "aws_lb_listener" "http" {
         status_code = 404
       }
     }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb-sg"
+    description = "Allow inbound HTTP traffic"
+
+    # Allow inbound HTTP requests (Port 80)
+    ingress {
+        description = "HTTP Traffic"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Allow all outbound requests to allow for health checks
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
 }
