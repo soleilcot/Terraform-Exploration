0bfaf73578490d5d12f51057bf99ad9e0206bc48
Soleil Cotterell <soleilari@gmail.com>
2022-11-05 20:39:37 -0500

    Migrate single EC2 instance to subnet-distributed ASG

--- main.tf
+++ main.tf
@@ -2,20 +2,20 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-E0F
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 E0F
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "terraform-example"
+    #This prevents terraform from destroying the old launch config before creating a new one upon modification
+    #If this is not done, terraform will not be able to update the config, as the reference to the old config by the asg resource will prevent destruction
+    lifecycle {
+      create_before_destroy = true
     }
 }
 
@@ -30,3 +30,19 @@ resource "aws_security_group" "instance" {
     }
   
 }
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnet.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+      key = "Name"
+      value = "terraform-asg-example"
+      propagate_at_launch = true
+    }
+}
+
+
